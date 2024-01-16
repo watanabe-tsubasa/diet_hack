464df3637a45cf7a41097ca71e19f466e650266f
watanabe-tsubasa <t.watanabe423@gmail.com>
2024-01-16 19:44:35 +0900

    upd

--- my-app/src/app.tsx
+++ my-app/src/app.tsx
@@ -3,7 +3,7 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
-import "./app.css";
+import "./root.css";
 
 export default function App() {
   return (
--- my-app/src/components/Counter.tsx
+++ my-app/src/components/Counter.tsx
@@ -1,13 +1,15 @@
 import { createSignal } from "solid-js";
+import { Button } from "./ui/button";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button
-      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-      onClick={() => setCount(count() + 1)}
-    >
-      Clicks: {count()}
-    </button>
+    <div>
+      <Button
+        onClick={() => setCount(count() + 1)}
+      >
+        Clicks: {count()}
+      </Button>
+    </div>
   );
 }
